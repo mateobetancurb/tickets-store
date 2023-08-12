e271d810031105ac2d9f32625a8932d48eb997bd
Mateo <mateobetancurb@gmail.com>
2023-08-12 16:41:35 -0500

    rendering app routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Events } from "./components/Events";
-import { SignupForm } from "./components/SignupForm";
+import { MyRoutes } from "./routes";
 
 const App = () => {
-	const [searchTerm, setSearchTerm] = useState("");
-
-	const handleNavbarSearch = (term) => {
-		setSearchTerm(term);
-	};
-
-	return (
-		<>
-			{/* <Navbar onSearch={handleNavbarSearch} />
-			<Events searchTerm={searchTerm} /> */}
-			<SignupForm />
-		</>
-	);
+	return <MyRoutes />;
 };
 
 export { App };
