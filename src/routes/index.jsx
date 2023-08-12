99cf89da31b38060f1f60533038a0fe08fc120a1
Mateo <mateobetancurb@gmail.com>
2023-08-12 17:56:29 -0500

    routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Detail } from "../pages/Detail";
 import { NotFound } from "../pages/NotFound";
+import { Profile } from "../pages/Profile";
 
 const router = createBrowserRouter([
 	{
@@ -12,6 +13,20 @@ const router = createBrowserRouter([
 	{
 		path: "/detail/:eventId",
 		element: <Detail />,
+	},
+	{
+		path: "/profile",
+		element: <Profile />,
+		children: [
+			{
+				path: "my-info",
+				element: <p>my info</p>,
+			},
+			{
+				path: "liked-events",
+				element: <p>liked events</p>,
+			},
+		],
 	},
 ]);
 
