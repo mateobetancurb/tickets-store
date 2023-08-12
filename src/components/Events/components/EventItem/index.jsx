fbe5fc2a977277b0c0ec7b7d4dc0116a26f4e95e
Mateo <mateobetancurb@gmail.com>
2023-08-12 16:42:53 -0500

    tailwind styles

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -5,11 +5,27 @@ const EventItem = ({ id, info, name, image, onEventClick }) => {
 	};
 
 	return (
-		<div onClick={() => console.log("padre click")}>
-			<img src={image} alt={name} width={200} height={200} />
-			<h4>{name}</h4>
-			<p>{info}</p>
-			<button onClick={handleSeeMoreClick}>Ver más</button>
+		<div
+			onClick={() => console.log("padre click")}
+			className="flex w-4/5 mx-auto mb-3 gap-5"
+		>
+			<img
+				src={image}
+				alt={name}
+				width={200}
+				height={200}
+				className="rounded-md"
+			/>
+			<div>
+				<h4 className="font-bold text-lg">{name}</h4>
+				<p className="text-sm">{info}</p>
+				<button
+					onClick={handleSeeMoreClick}
+					className="bg-[#00000034] px-3 py-2 rounded-md shadow-md mt-5 hover:bg-[#00000095] transition-colors"
+				>
+					Ver más
+				</button>
+			</div>
 		</div>
 	);
 };
