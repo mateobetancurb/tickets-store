30665f7163875e13a687b905bd6fe0421a05fe03
Mateo <mateobetancurb@gmail.com>
2023-08-13 11:38:04 -0500

    get event details id from api

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
 	const { eventId } = useParams();
-	console.log(eventId);
+	const [eventDetails, setEventDetails] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
+
+	const getEventDetailApi = async () => {
+		try {
+			const url = ` https://app.ticketmaster.com/discovery/v2/events/${eventId}/?apikey=${
+				import.meta.env.VITE_CONSUMER_KEY
+			}`;
+			const req = await fetch(url);
+			if (!req.ok) {
+				throw new Error("Error HTTP:" + req.status);
+			}
+			const res = await req.json();
+			setEventDetails(res);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getEventDetailApi();
+	}, []);
+
+	if (isLoading && Object.keys(eventDetails) === 0) {
+		return <p>Cargando detalles del evento...</p>;
+	}
 
 	return (
 		<>
